1847d5abee8e5eb681e89069391b40c8f2eb93e6
Satyam Khatiwada <satyamkhatiwada10@gmail.com>
2024-10-23 15:05:13 +0545

    Update Day 4.sql

--- Queries/Day 4.sql
+++ Queries/Day 4.sql
@@ -2,3 +2,10 @@
 SELECT * 
 FROM FILM
 WHERE rating = 'R';
+
+-- List all customers who live in a city with the name 'tabriz'. --
+SELECT cst.first_name, cst.last_name, a.address, c.city_id, c.city
+FROM customer cst
+JOIN address a on a.address_id = cst.address_id
+JOIN city c on c.city_id = a.city_id
+WHERE c.city = 'Tabriz';
