a89baf518272682e564d04725bb713ac7c8b0bdd
Satyam Khatiwada <satyamkhatiwada10@gmail.com>
2024-10-25 14:26:58 +0545

    Create Day 6.sql

---
+++ Queries/Day 6.sql
@@ -0,0 +1,10 @@
+--15	Find the staff members who processed the highest number of rentals.--
+
+
+--16	Retrieve the details of films that cost less than $10 to rent.--
+
+
+--17	Find all customers who registered in the last 12 months.--
+
+
+--18	Get the list of actors who have not acted in any films.--
