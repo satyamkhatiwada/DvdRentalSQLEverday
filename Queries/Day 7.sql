7b99afc786bce1fc3b90f13e6a945a5c0c63c1b1
Satyam Khatiwada <satyamkhatiwada10@gmail.com>
2024-10-25 14:54:31 +0545

    19 complete

--- Queries/Day 7.sql
+++ Queries/Day 7.sql
@@ -1,1 +1,16 @@
 --18	Get the list of actors who have not acted in any films.--
+SELECT a.actor_id, a.first_name
+FROM actor a
+LEFT JOIN film_actor fa on fa.actor_id = a.actor_id
+WHERE fa.actor_id is null;
+
+--19 Find the country with the highest number of customers.--
+SELECT count(cus.customer_id) as total_customer, cou.country_id, cou.country
+FROM country cou
+JOIN city c on c.country_id = cou.country_id
+JOIN address a on a.city_id = c.city_id
+JOIN customer cus on cus.address_id = a.address_id
+GROUP BY cou.country_id, cou.country
+ORDER BY total_customer DESC;
+
+--20	Retrieve the titles of all films with a rental rate greater than $5.--
