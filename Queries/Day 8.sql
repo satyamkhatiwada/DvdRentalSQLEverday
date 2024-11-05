ea28121f1ab63a9d65f4d992c5bae492f8e4c285
Satyam Khatiwada <satyamkhatiwada10@gmail.com>
2024-11-05 16:12:37 +0545

    Day 9 initial
    Questions updated

--- Queries/Day 8.sql
+++ Queries/Day 8.sql
@@ -7,14 +7,25 @@ JOIN city c ON c.city_id = a_cus.city_id
 JOIN address a_store ON a_store.city_id = c.city_id
 JOIN store s ON s.address_id = a_store.address_id;
 
---21	Find all stores located in the same city as customer ID 5.--
+--21B. Find all stores located in the same city as customer ID 5.--
+SELECT s.store_id, cus.customer_id, c.city_id, c.city
+FROM customer cus
+JOIN address a_cus ON cus.address_id = a_cus.address_id
+JOIN city c ON c.city_id = a_cus.city_id
+JOIN address a_store ON a_store.city_id = c.city_id
+JOIN store s ON s.address_id = a_store.address_id
+WHERE cus.customer_id = 5;
 
 
 --22	Retrieve the details of all payments made on the current date.--
-
+SELECT *
+FROM Payment p
+WHERE DATE(payment_date) = CURRENT_DATE;
 
 --23	Get the names of all staff members who have email addresses that end in '.org'.--
-
+SELECT staff_id, first_name, email
+FROM staff
+WHERE email LIKE '%.org';
 
 --24	Find the total number of rentals made by customer with ID 100.--
 
---
+++ Queries/Day 9.sql
@@ -0,0 +1,4 @@
+--24	Find the total number of rentals made by customer with ID 100.--
+
+
+--25	List the films that have a title containing the word 'Love'.--
