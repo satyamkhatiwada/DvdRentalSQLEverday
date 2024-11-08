97d5a2d8be4ee334148c712b76330af3493eabc5
Satyam Khatiwada <satyamkhatiwada10@gmail.com>
2024-11-08 14:38:19 +0545

    Update Day 9.sql

--- Queries/Day 9.sql
+++ Queries/Day 9.sql
@@ -1,4 +1,10 @@
 --24	Find the total number of rentals made by customer with ID 100.--
-
+SELECT c.customer_id, c.first_name, r.rental_id, r.rental_date, r.staff_id
+FROM customer c
+JOIN rental r ON c.customer_id = r.customer_id
+WHERE c.customer_id = 100;
 
 --25	List the films that have a title containing the word 'Love'.--
+SELECT *
+FROM film
+where LOWER(title) like '%love%';
