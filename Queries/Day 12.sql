9717d98d6894fda2e74870e1853109a7e7059331
Satyam Khatiwada <satyamkhatiwada10@gmail.com>
2024-11-12 13:15:31 +0545

    Additional Questions

--- Queries/Day 12.sql
+++ Queries/Day 12.sql
@@ -7,3 +7,11 @@ GROUP BY c.city_id, c.city
 ORDER BY total_staffs desc LIMIT 3;
 
 -- 32	List the films that have both 'Deleted Scenes' and 'Behind the Scenes' as special features.
+SELECT special_features, title
+FROM Film
+WHERE special_features @>ARRAY['Deleted Scenes', e'Behind the Scenes'];
+
+-- 33	Find all customers who rented films from store ID 2.
+SELECT customer_id, store_id, first_name
+FROM customer
+WHERE store_id = 2;
