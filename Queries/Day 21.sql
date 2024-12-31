eaa0b05783d281ce9dfc9f33b72074ad202b641d
Satyam Khatiwada <99595892+satyamkhatiwada@users.noreply.github.com>
2024-12-31 12:03:18 +0545

    First of day 21

--- Queries/Day 21.sql
+++ Queries/Day 21.sql
@@ -1,3 +1,13 @@
 -- 56	Find the total number of rentals processed by each staff member.
-
+SELECT 
+    s.staff_id, 
+    COUNT(r.rental_id) AS total_rentals
+FROM 
+    staff s
+LEFT JOIN 
+    rental r 
+ON 
+    s.staff_id = r.staff_id
+GROUP BY 
+    s.staff_id;
 -- 57	Get the list of films released after the year 2005.
